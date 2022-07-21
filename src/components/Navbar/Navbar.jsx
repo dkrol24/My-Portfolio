8ff1f19f453affcff65ba173662cf93a3cee6975
dkrol24 <krold2191@gmail.com>
2022-07-21 16:27:17 +0200

    initial

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React,{useState} from 'react'
+import {HiMenu, HiX} from 'react-icons/hi'
+import {motion} from 'framer-motion'
+
+import './Styles.scss'
+import images from '../../constants/images'
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <h1 className='testing'>David</h1>
+        
+        <ul className="app__navbar-links">
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+        <div className="app__navbar-menu">
+            <HiMenu onClick={() => setToggle(true)}/>
+
+            {toggle && (
+              <motion.div  animate={{
+                x: [300,0],
+                position: "fixed",
+              }}>
+                <HiX onClick={()=> setToggle(false)}/>
+                <ul className='app__navbar-links'>
+                {['home', 'about','work','skills','contact'].map((item) => (
+                  <li className='app__flex p-text' key={item}>
+                  <a href={`#${item}`} onClick={()=> setToggle(false)}>{item}
+                  <div className='app__navbar-ball'/>
+                  </a>
+                  </li>
+          ))}
+          </ul>
+              </motion.div>
+            )}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState} from 'react';
+import emailjs from '@emailjs/browser';
+import  images  from '../../constants/images';
+import { AppWrap, MotionWrap } from '../../wrapper';
+
+import './Styles.scss';
+
+const Footer = () => {
+  const [values, setValues] = useState({
+    fullName: '',
+    email: '',
+     message: '',
+   });
+   const [status, setStatus] = useState(false)
+   const [loading, setLoading] = useState(false);
+   
+ 
+ const handleChange = (e) => {
+   setValues(values => ({
+     ...values,
+     [e.target.name]: e.target.value
+   }))
+ };
+ const handleSubmit = (e) => {
+   e.preventDefault();
+   emailjs.send('service_vp66n7q', 'template_awe19s9', values, 'smQiWYotfaIg-1_RE')
+   .then(response => {
+     console.log('Wiadomość wysłana!', response)
+     setValues({
+       fullName: '',
+      email: '',
+       message: '',
+     });
+     setStatus(true);
+     setLoading(true);
+   },error=>{
+     console.log('Podaj dane i zaakceptuj regulamin.', error)
+   })
+ };
+ 
+
+
+  return (
+    <>
+      <h2 className="head-text">Get in touch <span>||</span> chat with me</h2>
+
+      
+      {!status ? (
+         <form  onSubmit={handleSubmit} className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your Name" name="fullName" value={values.fullName} onChange={handleChange} />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={values.email} onChange={handleChange} />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={values.message}
+              name="message"
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="p-text">Send</button>
+        </form>
+      ) : (
+        <div>
+          <h3 className="head-text">
+            Thank you for getting in touch!
+          </h3>
+        </div>
+      )}
+      <div className="app__footer-cards">
+        <div className="app__footer-card ">
+          <img src={images.email} alt="email" />
+          <a href="mailto:krold2191@gmail.com" className="p-text">krold2191@gmail.com</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+48 530-764-578" className="p-text">530-764-578</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg',
+);
